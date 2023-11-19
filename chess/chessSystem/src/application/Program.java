7f5214215f9298c0a10e5a607a2075ed26b36b6b
DjalmaOlivei <djalma.costa.filho@gmail.com>
2023-11-18 22:28:29 -0300

    chess - Piece Board

--- chess/chessSystem/src/application/Program.java
+++ chess/chessSystem/src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
+import boardgame.Board;
 import  boardgame.Position;
 
 public class Program {
 	
 	public static void main(String args[]) {
 		Position p = new Position(0, 0);
+		Board board = new Board(8, 8);
 		System.out.println(p.toString());
 	}
 
