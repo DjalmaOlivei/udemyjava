dd0f34776dd9a9792969a50834edaf182e787940
DjalmaOlivei <djalma.costa.filho@gmail.com>
2023-11-19 02:11:58 -0300

    chess - possible moves

--- chess/chessSystem/src/chess/pieces/King.java
+++ chess/chessSystem/src/chess/pieces/King.java
@@ -8,12 +8,18 @@ public class King extends ChessPiece {
 
     public King(Board board, Color color) {
         super(board, color);
-        //TODO Auto-generated constructor stub
+       
     }
 
     @Override
     public String toString(){
         return "K";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColums()];
+        return mat;
     }
     
 }
--- chess/chessSystem/src/chess/pieces/Rook.java
+++ chess/chessSystem/src/chess/pieces/Rook.java
@@ -8,12 +8,18 @@ public class Rook extends ChessPiece {
 
     public Rook(Board board, Color color) {
         super(board, color);
-        //TODO Auto-generated constructor stub
+    
     }
 
     @Override
     public String toString(){
         return "R";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColums()];
+        return mat;
     }
     
 }
